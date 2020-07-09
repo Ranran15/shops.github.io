70ed35ce94d92294e0eaaf0145494ddff4873461
Ranran15 <rrui1510@gmail.com>
2020-07-09 16:52:29 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function alertBuy() {
+  var products = document.getElementsByName("product");
+  var product = "A";
+  for (var i = 0, length = products.length; i < length; i++) {
+    if (products[i].checked) {
+      product = products[i].value;
+      break;
+    }
+  }
+  
+  var productNum = document.getElementById("numSelect").value;
+  
+  alert("You've just bought " + productNum + " " + product + "(s)!");
+}
