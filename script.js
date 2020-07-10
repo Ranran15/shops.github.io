c634b09e9f81ffd0a92816c6fd4ddab5161fcf3d
Ranran15 <rrui1510@gmail.com>
2020-07-10 10:17:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function alertBuy() {
   var products = document.getElementsByName("product");
   var product = "A";
+  var price = 10;
   for (var i = 0, length = products.length; i < length; i++) {
     if (products[i].checked) {
       product = products[i].value;
+      price += i*10;
       break;
     }
   }
   
   var productNum = document.getElementById("numSelect").value;
+  
+  fbq('track', 'Purchase', {currency: "USD", value: price});
   
   alert("You've just bought " + productNum + " " + product + "(s)!");
 }
