8eb56c385e63b430fd8b05e0df67706e04bf1b72
Ranran15 <rrui1510@gmail.com>
2020-10-02 23:44:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function alertBuy() {
   
   
-  var products = document.getElementsByName("product");
-  var product = "A";
-  var price = 10;
-  var product_item_id = 11111;
-  for (var i = 0, length = products.length; i < length; i++) {
-    if (products[i].checked) {
-      product = products[i].value;
-      price += i*10;
-      product_item_id *= (i+1);
-      break;
-    }
-  }
+//   var products = document.getElementsByName("product");
+//   var product = "A";
+//   var price = 10;
+//   var product_item_id = 11111;
+//   for (var i = 0, length = products.length; i < length; i++) {
+//     if (products[i].checked) {
+//       product = products[i].value;
+//       price += i*10;
+//       product_item_id *= (i+1);
+//       break;
+//     }
+//   }
   
-  var productNum = document.getElementById("numSelect").value;
+ // var productNum = document.getElementById("numSelect").value;
       fbq('track', 'Purchase', {currency: "USD", value: price, content_type:"product", contents:[
       {id:"DB_1",
       quantity:1},{id:"DB_2",quantity:2}]});
@@ -32,6 +32,6 @@ function alertBuy() {
 //         }],
 //       content_type: 'product',
 //     });
-  
-  alert("You've just bought " + productNum + " " + product + "(s)!");
+  alert("谢谢大佬“);
+  //alert("You've just bought " + productNum + " " + product + "(s)!");
 }
