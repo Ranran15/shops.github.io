4b1ddfdde631743e903ed2a20fa52bd96c9048a4
Ranran15 <rrui1510@gmail.com>
2020-10-06 15:46:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 function alertBuy() {
   
   
-  var products = document.getElementsByName("product");
-  var product = "A";
-  var price = 10;
-  var product_item_id = 11111;
-  for (var i = 0, length = products.length; i < length; i++) {
-    if (products[i].checked) {
-      product = products[i].value;
-      price += i*10;
-      product_item_id *= (i+1);
-      break;
-    }
-  }
+//   var products = document.getElementsByName("product");
+//   var product = "A";
+//   var price = 10;
+//   var product_item_id = 11111;
+//   for (var i = 0, length = products.length; i < length; i++) {
+//     if (products[i].checked) {
+//       product = products[i].value;
+//       price += i*10;
+//       product_item_id *= (i+1);
+//       break;
+//     }
+//   }
   
-  var productNum = document.getElementById("numSelect").value;
-      fbq('track', 'Purchase', {currency: "USD", value: price, content_type:"product", contents:[
-      {id:"DB_1",
-      quantity:1},{id:"DB_2",quantity:2}]});
-  fbq('track', 'Purchase', {
-      value: 10,
-      currency: 'USD',
-      contents: [
-        {
-          id: 'DB_1',
-          quantity: 1
-        },
-        {
-          id: 'DB_2',
-          quantity: 2
-        }],
-      content_type: 'product',
-    });
-  alert("You've just bought " + productNum + " " + product + "(s)!");
+//   var productNum = document.getElementById("numSelect").value;
+//       fbq('track', 'Purchase', {currency: "USD", value: price, content_type:"product", contents:[
+//       {id:"DB_1",
+//       quantity:1},{id:"DB_2",quantity:2}]});
+//   fbq('track', 'Purchase', {
+//       value: 10,
+//       currency: 'USD',
+//       contents: [
+//         {
+//           id: 'DB_1',
+//           quantity: 1
+//         },
+//         {
+//           id: 'DB_2',
+//           quantity: 2
+//         }],
+//       content_type: 'product',
+//     });
+  alert("thanks!!!");
+  //alert("You've just bought " + productNum + " " + product + "(s)!");
 }
